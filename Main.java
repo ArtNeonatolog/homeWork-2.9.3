558fd2e1c8eb95465ceb605f3d67e30bbf7b3f64
Артем <marcus201@mail.ru>
2022-11-26 18:46:28 +0500

    Задание выполнено, используя видеоразбор домашки. Но возникла проблема (даже 2) при попытке добавить еще один идентичный ключ и значение исключение не выходит, если вытаешься добавить новый ключ и значение джава выкидывает исключение nullPointException. В чем причина?

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+            Task3 task = new Task3();
+
+            task.addMap();
+        System.out.println(task);
+
+            task.changeMap("Иван", 456989);
+
+                   System.out.println(task);
+
+        }
     }
-}
+
---
+++ Task3.java
@@ -0,0 +1,49 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class Task3 {
+    private Map<String, Integer> collection = new HashMap<>();
+
+    public void addMap () {
+        collection.put("Иван", 123456);
+        collection.put("Петр", 723486);
+        collection.put("Артем", 423756);
+        collection.put("Денис", 143456);
+        collection.put("Роман", 223456);
+    }
+
+    public void changeMap(String key, int value) {
+
+        if (collection.containsKey(key)) {
+            collection.put(key, value);
+            } else {
+            int valueOfMap = collection.get(key);
+            if (valueOfMap == value) {
+                throw new IllegalArgumentException();
+            } else {
+                collection.put(key, value + valueOfMap);
+            }
+        }
+            }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task3 task3 = (Task3) o;
+        return Objects.equals(collection, task3.collection);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(collection);
+    }
+
+    @Override
+    public String toString() {
+        return "Task3{" +
+                "collection=" + collection +
+                '}';
+    }
+}
